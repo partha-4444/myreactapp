8fbc9fc247bea9ad1b7a37f2ccc180508088f543
partha-4444 <partha2031.be22@chitkara.edu.in>
2025-05-02 16:53:17 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>My react app</h1>
+        <h2>my react implement with CI/CD</h2>
+        <h3>Partha Venom</h3>
+        
+      </div>
+     
+    </>
+  )
+}
+
+export default App
